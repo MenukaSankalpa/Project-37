ac4062f409c58319a117778e02dea2ae8b73bda9
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-02-13 10:47:34 +0530

    add photo and label

--- GUI.py
+++ GUI.py
@@ -4,5 +4,18 @@ root = Tk()
 root.title("AI Assistant")
 root.geometry("550x675")
 root.resizable(False, False)
+root.config(bg="#6F8FAF")
+
+frame = LabelFrame(root, padx=100, pady=7, borderwidth=3, relief="raised")
+frame.config(bg="#6F8FAF")
+frame.grid(row=0, column=1,padx=55,pady=10)
+
+text_label = Label(frame, text="AI Assistance",font=("comic Sans ms", 14,"bold"),bg="#356696")
+text_label.grid(row=0,column=0,padx=20,pady=10)
+
+image = ImageTk.PhotoImage(Image.open("image/assitant.png"))
+image_label = Label(frame,image=image)
+image_label.grid(row=1,column=0,pady=20)
+
 
 root.mainloop()
