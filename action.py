a1cefdb301bcbb4a831cd664c4f046b954caca7e
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-02-14 10:54:12 +0530

    def action made

--- action.py
+++ action.py
@@ -7,7 +7,20 @@ def Action():
     user_data = speech_to_text.speech_to_text()
     
     if "your name" in user_data:
-        text_to_speech.text_to_speech("my name is AI Desktop vertual assistant")
-        return "my name is Ai Desktop vertual assistant"
+        text_to_speech.text_to_speech("my name is AI Desktop virtual assistant")
+        return "my name is Ai Desktop virtual assistant"
+    elif "hello" in user_data or "hi" in user_data:
+        text_to_speech.text_to_speech("hey, sir how can i help you")
+        return 'hey, sir how can i help you'
+    elif "Good morning" in user_data:
+        text_to_speech.text_to_speech("good morning sir")
+    elif "what is the time now" in user_data:
+        current_time = datetime.datetime.now()
+        Time = (str)(current_time.hour) + "Hour :",(str)(current_time.minute) + "Minute"
+        text_to_speech.text_to_speech(Time)
+        return Time
+    
+    elif "shutdown" in user_data:
+        text_to_speech.text_to_speech("ok sir")
     
 Action()    
